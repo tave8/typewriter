cb75e1c4fef5e61c9bdc9ab427a53f8c85ab4468
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-07 02:27:53 +0100

    feature "typewrite one at a time" & readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Typewriting
+
+## Features
+
+
+### Human-like typewriting
+
+Random intervals between character typing allow for human-like typewriting.
+
+### Typewrite in bulk
+
+An array of objects in which you specify the html element and the text to be typewrited, are all that you need.
+
+### Typewrite with callback on finish typewriting
+
+On the event finish typewrite, you can call a custom callback.
+
+### Typewrite "one at a time"
+
+Prevent the typewrite feature to be called more than once while it's "busy". Consecutive calls to the typewriting will have no effect 
+until the typewriting function will have finished its work.
--- script.js
+++ script.js
@@ -1,38 +1,88 @@
-
-
-
 function startTypewriteInteraction() {
-  typeWriteMultipleElements([
-    {elId: "p1", text: "il bicchiere mezzo pieno o mezzo intero"},
-    {elId: "p2", text: "chi va piano va sano e va vicino"},
-    {elId: "p3", text: "amore a primo olfatto"},
-    {elId: "p4", text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ex quidem repellat ullam ad, consectetur, labore facilis, nihil suscipit cum necessitatibus maiores consequatur alias fuga qui dolorem obcaecati corrupti sint."},
+  function callbackOnFinish() {
+    // alert("finito!");
+  }
 
-  ])
+  typeWriteMultipleElements([
+    { elId: "p1", text: "il bicchiere mezzo pieno o mezzo intero", callbackOnFinish },
+    // { elId: "p2", text: "chi va piano va sano e va vicino" },
+    // { elId: "p3", text: "amore a primo olfatto" },
+    // {
+    //   elId: "p4",
+    //   text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta ex quidem repellat ullam ad, consectetur, labore facilis, nihil suscipit cum necessitatibus maiores consequatur alias fuga qui dolorem obcaecati corrupti sint.",
+    // },
+  ]);
 }
 
 window.addEventListener("load", () => {
   startTypewriteInteraction();
-
 });
 
-
-
 function typeWriteMultipleElements(elements) {
+  let func = null
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
-    const elHtml = document.getElementById(element.elId)
-    typeWrite(element.text, elHtml)
+    const elHtml = document.getElementById(element.elId);
+    const callbackOnFinish = element.callbackOnFinish;
+    const fn = typeWriteOneAtTime(element.text, elHtml, { callbackOnFinish });
+    // typeWriteOneAtTime(element.text, elHtml, { callbackOnFinish });
+    // typeWriteOneAtTime(element.text, elHtml, { callbackOnFinish });
+    fn();
+    fn();
+    fn();
+    // func = fn
   }
+
+  // setTimeout(func, 10000)
+
 }
 
+/**
+ * Run the typewriting function only one at a time,
+ * meaning that the function cannot be re-run until
+ * it has finished the "previous" round of typewriting.
+ * This prevents weird situations like a text container
+ * having the typewriting functionality multiple times,
+ * which means the text will be typewrite (we assume) faster
+ * for each extra typewrite function call.
+ * Successive function calls of typeWriteOneAtTime
+ * will have no effect, until the last typewriting is complete.
+ */
+function typeWriteOneAtTime(text, htmlEl, { options = {}, callbackOnFinish = null }) {
+  // the first time the function gets called, typewrite is available
+  let isTypewriteAvailable = true;
 
-function typeWrite(text, htmlEl, options = {}) {
+  return function () {
+    // this inner function allows the value of the outer function
+    // to be changed on demand, even outside of this outer function
+    function setTypewriteAvailable(val) {
+      isTypewriteAvailable = val;
+    }
+
+    if (isTypewriteAvailable) {
+      typeWrite(text, htmlEl, { options, callbackOnFinish }, setTypewriteAvailable);
+    } else {
+      console.log(
+        "You are calling the typewrite function in 'one at a time' mode, " +
+          "which means you can call the typewrite function again, " +
+          "only when it has finished all its typewring."
+      );
+    }
+  };
+}
+
+function typeWrite(text, htmlEl, { options = {}, callbackOnFinish = null }, setTypewriteAvailableWhenOneAtTime = null) {
+  // set the typewriter as not available, so
+  // this function cannot be recalled, only when it was called
+  // from the function "typewrite one at a time"
+  if (setTypewriteAvailableWhenOneAtTime) {
+    setTypewriteAvailableWhenOneAtTime(false);
+  }
+
   const nChars = text.length;
   let lastTimeout = 0;
   const timeoutStep = 10;
   const cursorExists = true;
-  const promises = []
 
   for (let i = 0; i < nChars; i++) {
     const newText = cutText(text, i + 1);
@@ -40,15 +90,31 @@ function typeWrite(text, htmlEl, options = {}) {
 
     setTimeout(() => {
       updateText(newText, htmlEl, cursorExists);
-    }, lastTimeout)
+    }, lastTimeout);
 
     lastTimeout += timeoutStep + randomTimeout;
-
   }
 
+  // this simulates an asynchronous operation,
+  // without blocking the function's execution.
+  // it's simply a timeout with timeout equal
+  // to the last saved timeout
   setTimeout(() => {
     removeCursorIfExists(htmlEl, cursorExists);
-  }, lastTimeout)
+
+    // operationFinished = true;
+    // if there's a callback, run it
+    if (callbackOnFinish) {
+      callbackOnFinish();
+    }
+
+    // if the typewrite function was called from the
+    // "one at a time" function, then this callback should be set
+    // from "one at a time" function
+    if (setTypewriteAvailableWhenOneAtTime) {
+      setTypewriteAvailableWhenOneAtTime(true);
+    }
+  }, lastTimeout);
 }
 
 function updateText(text, htmlEl, addCursor = false) {
@@ -60,7 +126,6 @@ function cutText(text, iUntil) {
   return text.slice(0, iUntil);
 }
 
-
 function removeCursorIfExists(htmlEl, cursorExists) {
   if (cursorExists) {
     htmlEl.textContent = htmlEl.textContent.slice(0, -1);
@@ -70,8 +135,3 @@ function removeCursorIfExists(htmlEl, cursorExists) {
 function getRandomTimeout(start = 10, end = 50) {
   return Math.floor(Math.random() * (end - start + 1)) + start;
 }
-
-
-
-
-
